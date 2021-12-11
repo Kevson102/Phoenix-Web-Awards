685e4e7e433d53b2abfd7d7afa9a637b74708c34
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-11 12:51:31 +0300

    CREATES A PROJECT DETAILS PAGE TOGETHER WITH ITS VIEW AND URL

--- webawards/urls.py
+++ webawards/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='home')
+    url(r'^$', views.home, name='home'),
+    url(r'^detailed-Project/', views.project_details)
 ]
--- webawards/views.py
+++ webawards/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+def project_details(request):
+  return render(request, 'ProjectDetails.html')
