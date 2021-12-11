bc3c1e179de1a46a6e26df416650693c4bb8140b
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-11 13:13:16 +0300

    CREATES A PROFILE MODEL

--- webawards/models.py
+++ webawards/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+  profile_photo = models.ImageField(upload_to='static/images/')
+  user_bio = models.TextField()
+  email = models.EmailField()
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.user_bio
