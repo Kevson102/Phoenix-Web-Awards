1f0afb2be52ce1a3ea0efa14800c4e43c98548bb
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-11 08:00:33 +0300

    INITIAL COMMIT WITH THE PROJECT AND APP CREATED

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kevson/Desktop/Core/Django/Independent Projects/Week 3/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
