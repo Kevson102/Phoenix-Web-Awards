b3904828d4a3fcb5042d030757670290862e4c70
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-11 18:10:01 +0300

    ADDS A LOOP IN THE INDEX.HTML FILE TO DISPLAY ALL THE POSTED PROJECTS AND LINKS EVERY PROJECT TO THE PROJECT DETAILS PAGE

--- webawards/urls.py
+++ webawards/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^detailed-Project/', views.project_details),
     url(r'^accounts/profile/', views.profile, name='profile'),
+    url(r'^details/(?P<project_id>\d+)', views.project_details, name='project_details'),
 ]
--- webawards/views.py
+++ webawards/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import Http404
 
 from webawards.models import Project
 
@@ -7,8 +8,13 @@ def home(request):
   projects=Project.get_all_projects()
   return render(request, 'index.html', {"projects":projects})
 
-def project_details(request):
-  return render(request, 'ProjectDetails.html')
+def project_details(request, project_id):
+  try:
+    project_details = Project.objects.get(pk = project_id)
+  except DoesNotExist:
+    raise Http404
+  
+  return render(request, 'ProjectDetails.html', {"project_details":project_details})
 
 def profile(request):
   return render(request, 'profile.html')
