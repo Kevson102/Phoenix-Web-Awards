269485cc18cb740b31fa4402446e6de814782780
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-12 08:14:15 +0300

    CREATES A VOTES MODEL AND MAKES A MIGRATION

--- webawards/models.py
+++ webawards/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Profile(models.Model):
@@ -42,3 +43,28 @@ class Project(models.Model):
   
   def __str__(self):
     return self.project_title
+
+class Votes(models.Model):
+  design_score = models.IntegerField(default=1,
+                                     validators=[
+                                       MaxValueValidator(10),
+                                       MinValueValidator(1)
+                                     ]
+                                     )
+  usability_score = models.IntegerField(default=1,
+                                     validators=[
+                                       MaxValueValidator(10),
+                                       MinValueValidator(1)
+                                     ]
+                                     )
+  content_score = models.IntegerField(default=1,
+                                     validators=[
+                                       MaxValueValidator(10),
+                                       MinValueValidator(1)
+                                     ]
+                                     )
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  project = models.ForeignKey(Project, on_delete=models.CASCADE)
+  
+  def __str__(self):
+    return self.pk
