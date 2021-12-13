af0a7bd1688dac23ba92d28d0310dbc65dc74471
Kevson102 <nyambura.njuguna@student.moringaschool.com>
2021-12-13 10:37:41 +0300

    CORRECTS THE BUG IN THE SETTINGS FILE

--- phoenixwebawards/settings.py
+++ phoenixwebawards/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 from decouple import config, Csv
 import dj_database_url
-import django_on_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,4 +173,4 @@ MEDIA_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
-django_on_heroku.settings(locals())
+django_heroku.settings(locals())
